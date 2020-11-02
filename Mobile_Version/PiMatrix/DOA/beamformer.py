98751d13503284d3b58645d80e9f310101d8612f
Darren <tohtingyu94@gmail.com>
2020-11-02 21:21:55 +0800

    change DOA test and DOA

---
+++ Mobile_Version/PiMatrix/DOA/beamformer.py
@@ -0,0 +1,10 @@
+"""
+takes in audio, passes it to the DOA to get the corresponding direction of arrivals then adjusts the gain levels accordingly
+
+
+"""
+
+
+def beamform():
+    # min value -32,768 to 32,767
+    print()
